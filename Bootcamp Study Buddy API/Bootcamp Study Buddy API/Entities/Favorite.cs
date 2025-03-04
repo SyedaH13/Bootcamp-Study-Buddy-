93cd2727e3db8972b38c4853735ae882c7a4215d
rebeccah-barlas <rebeccah.m.barlas@gmail.com>
2025-03-03 21:36:43 -0500

    added controllers

---
+++ Bootcamp Study Buddy API/Bootcamp Study Buddy API/Entities/Favorite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bootcamp_Study_Buddy_API.Entities;
+
+public partial class Favorite
+{
+    public int Id { get; set; }
+
+    public int? FavoriteId { get; set; }
+
+    public virtual Question? FavoriteNavigation { get; set; }
+}
--- Bootcamp Study Buddy API/Bootcamp Study Buddy API/Entities/StudyBuddyContext.cs
+++ Bootcamp Study Buddy API/Bootcamp Study Buddy API/Entities/StudyBuddyContext.cs
@@ -6,13 +6,42 @@ namespace Bootcamp_Study_Buddy_API.Entities;
 
 public partial class StudyBuddyContext : DbContext
 {
+    public StudyBuddyContext()
+    {
+    }
+
     public StudyBuddyContext(DbContextOptions<StudyBuddyContext> options)
         : base(options)
     {
     }
 
+    public virtual DbSet<Favorite> Favorites { get; set; }
+
+    public virtual DbSet<Question> Questions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Favorite>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Favorite__3214EC0788181FBF");
+
+            entity.HasOne(d => d.FavoriteNavigation).WithMany(p => p.Favorites)
+                .HasForeignKey(d => d.FavoriteId)
+                .HasConstraintName("FK__Favorites__Favor__398D8EEE");
+        });
+
+        modelBuilder.Entity<Question>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Question__3214EC27A8204FBC");
+
+            entity.ToTable("Question");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
