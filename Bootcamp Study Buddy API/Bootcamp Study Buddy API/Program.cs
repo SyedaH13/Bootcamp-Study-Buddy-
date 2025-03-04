6021b7396dc172029cb5d50a489cc15b1b42833a
rebeccah-barlas <rebeccah.m.barlas@gmail.com>
2025-03-03 19:34:36 -0500

    added API and git ignore file

---
+++ Bootcamp Study Buddy API/Bootcamp Study Buddy API/Program.cs
@@ -0,0 +1,45 @@
+using Bootcamp_Study_Buddy_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+
+        });
+    });
+
+IConfigurationBuilder buildConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+IConfiguration configuration = buildConfig.Build();
+builder.Services.AddDbContext<StudyBuddyContext>(options => options.UseSqlServer(configuration.GetConnectionString("StudyBuddy")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+app.UseCors();
